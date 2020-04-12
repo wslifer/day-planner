8531f1a2572a7614a8742658c12c227761af71eb
Wesley Slifer <wesleyslifer@ymail.com>
2020-04-12 13:24:23 -0500

    updated each function. added click for save button and icon. made function to get current time.

--- assets/script.js
+++ assets/script.js
@@ -17,14 +17,45 @@ var workHours = [
   "5PM",
 ];
 var container = $(".container");
-workHours.forEach(function (time) {
-  var text = $("<textarea>").attr("dataStorage", time);
-  var btn = $("<button>").addClass("saveBtn");
-  var row = $("<div>").addClass("row");
-  var hours = $("<div>").addClass("hour");
-  let any = parseInt(time);
-  hours.text(time);
-  row.append(hours, text, btn);
 
-  $(".container").append(row);
+$.each(workHours, function (index, value) {
+  var backgroundColor = getTime(value);
+  var newRow = $("<div>").addClass("row");
+  var newCol1 = $("<div>" + value + "</div>").addClass("col-sm-1 hour time");
+  var newCol2 = $("<div></div>").addClass(
+    "col-sm-10 past description " + backgroundColor + " time-" + value
+  );
+  var newCol3 = $("<button></button>").addClass("col-sm-1 saveBtn");
+  var textarea = $("<textarea></textarea>").addClass("col-sm-12 input");
+  var icon = $(
+    '<i class="far fa-save fa-3x" style="margin: auto; padding: 10px;"></i>'
+  );
+
+  newRow.append(newCol1);
+  newRow.append(newCol2);
+  newCol2.append(textarea);
+  newRow.append(newCol3);
+  newCol3.append(icon);
+  container.append(newRow);
+});
+
+function getTime(current) {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+
+  var newformat = hours >= 12 ? "PM" : "AM";
+
+  hours = hours % 12;
+
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var currentTime = hours + newformat;
+}
+$(".saveBtn").click(function () {
+  var time = $(this).siblings("div.time").text();
+  console.log(time);
+  var input = $(this).siblings("div.description").children("textarea").val();
+  console.log(input);
+  localStorage.setItem(time, input);
 });
