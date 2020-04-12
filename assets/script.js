c2e2b0a78c383e9db56bc1fb2322151135728df3
Wesley Slifer <wesleyslifer@ymail.com>
2020-04-12 09:56:18 -0500

    added timeblocks to page for each work hour

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,27 @@ window.onload = function () {
   // get a current day from page being loaded
   $("#currentDay").html(date);
 };
+
+var workHours = [
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+];
+var container = $(".container");
+workHours.forEach(function (time) {
+  var text = $("<textarea>").attr("dataStorage", time);
+  var btn = $("<button>").addClass("saveBtn");
+  var row = $("<div>").addClass("row");
+  var hours = $("<div>").addClass("hour");
+  let any = parseInt(time);
+  hours.text(time);
+  row.append(hours, text, btn);
+
+  $(".container").append(row);
+});
